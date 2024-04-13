5cb33259a28bdb2c534fb2c527ef2c7a5c22cf2e
Juan Jose Ospina Restrepo <juan.ospina645@asys.edu.co>
2024-04-12 19:04:16 -0500

    Subir 4to ejercicio

---
+++ ejercicio4.js
@@ -0,0 +1,7 @@
+let names = prompt("Por favor, introduce tu nombre:")
+let birthYear = prompt("Digite la fecha de nacimiento")
+
+let currentYear = 2024
+let edad = currentYear - birthYear
+
+console.log(`Hola ${names} tienes ${edad} años`);
